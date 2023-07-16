b119055e81c5ab906e071da10189ba8befad23e3
yummpotato <hevlyoy26@naver.com>
2023-07-17 03:46:03 +0900

    [UI] activity 구현 및 버튼 위젯 수정

--- app/src/main/java/com/gachon/ttuckttak/ui/setting/SettingsAlertFragment.kt
+++ app/src/main/java/com/gachon/ttuckttak/ui/setting/SettingsAlertFragment.kt
@@ -1,5 +1,6 @@
 package com.gachon.ttuckttak.ui.setting
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -16,7 +17,9 @@ class SettingsAlertFragment : Fragment(), View.OnClickListener {
 
         // 뒤로가기 버튼을 누르는 경우
         backButtonFrame.setOnClickListener {
-            findNavController().navigate(R.id.action_settingsAlertFragment_to_settingsFragment)
+           val mIntent = Intent(getActivity(), SettingsActivity::class.java)
+            mIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(mIntent)
         }
 
         // switch를 누르는 경우
