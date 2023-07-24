9c6c3c7645623075a5d838597a536e57c9349e73
yummpotato <hevlyoy26@naver.com>
2023-07-25 05:17:41 +0900

    [UI] 회원가입 화면 및 xml 구현

---
+++ app/src/main/java/com/gachon/ttuckttak/ui/join/JoinPart1Activity.kt
@@ -0,0 +1,65 @@
+package com.gachon.ttuckttak.ui.join
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.gachon.ttuckttak.R
+import com.gachon.ttuckttak.base.BaseActivity
+import com.gachon.ttuckttak.databinding.ActivityJoinPart1Binding
+import com.gachon.ttuckttak.ui.login.StartActivity
+
+class JoinPart1Activity : BaseActivity<ActivityJoinPart1Binding>(ActivityJoinPart1Binding::inflate) {
+    override fun initAfterBinding() = with(binding) {
+        // 인증코드 보내기 버튼 비활성화
+        buttonCertificationCode.setClickable(false)
+
+        // 뒤로가기 버튼을 눌렀을 경우
+        buttonBack.setOnClickListener {
+           // 시작 화면으로 이동
+            startActivityWithClear(StartActivity::class.java)
+       }
+
+        // 이메일 입력창을 클릭했을 경우
+        layoutJoinEmail.setOnClickListener {
+            // edittextEmail 값 가져오기
+            val email = edittextEmail.text.toString()
+
+            // email을 입력하는 경우 -- textbox 테두리 색 변경
+            layoutJoinEmail.setBackgroundResource(R.drawable.box_input_text)
+
+            // email이 중복일 경우 -- 임시설정
+            if(true) {
+                // textbox 테두리 및 글자 색 변경
+                layoutJoinEmail.setBackgroundResource(R.drawable.box_error_text)
+                edittextEmail.setTextColor(ContextCompat.getColor(applicationContext, R.color.general_theme_error))
+
+                // invisible 되어 있던 error message 보이기
+                textviewOverlapEmail.visibility = View.VISIBLE
+            }
+            // email이 중복이 아닐 경우
+            else {
+                // error message invisible하기
+                textviewOverlapEmail.visibility = View.INVISIBLE
+
+                // email이 적합할 경우 -- textbox 및 글자 색 변경하기
+                layoutJoinEmail.setBackgroundResource(R.drawable.box_input_text)
+                edittextEmail.setTextColor(ContextCompat.getColor(applicationContext, R.color.black))
+
+                // 인증코드 보내기 버튼 색 변경
+                buttonCertificationCode.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.main_theme_blue))
+                // 인증코드 보내기 버튼 글자 색 변경
+                buttonCertificationCode.setTextColor(getColor(R.color.white))
+                // 인증코드 보내기 버튼 활성화
+                buttonCertificationCode.setClickable(true)
+            }
+        }
+
+        // 인증코드 보내기 버튼을 눌렀을 경우
+        buttonCertificationCode.setOnClickListener {
+            // 입력한 email 값 전달하기
+            intent.putExtra("email", edittextEmail.text.toString())
+
+            // JoinPart2Activity로 이동
+            startNextActivity(JoinPart2Activity::class.java)
+        }
+    }
+}
