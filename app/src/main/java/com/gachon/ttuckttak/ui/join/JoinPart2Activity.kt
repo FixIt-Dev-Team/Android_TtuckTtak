02e0d8c7aca664bfc81f2407abf5d9fd55e136b0
yummpotato <hevlyoy26@naver.com>
2023-07-25 05:17:41 +0900

    [UI] 회원가입 화면 및 xml 구현

---
+++ app/src/main/java/com/gachon/ttuckttak/ui/join/JoinPart2Activity.kt
@@ -0,0 +1,89 @@
+package com.gachon.ttuckttak.ui.join
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.gachon.ttuckttak.R
+import com.gachon.ttuckttak.base.BaseActivity
+import com.gachon.ttuckttak.databinding.ActivityJoinPart2Binding
+import java.util.*
+import kotlin.concurrent.timer
+
+class JoinPart2Activity : BaseActivity<ActivityJoinPart2Binding>(ActivityJoinPart2Binding::inflate) {
+    var time = 0
+    var timerTask : Timer? = null
+
+    override fun initAfterBinding() = with(binding) {
+        // timer 시작
+        startTimer()
+
+        // 인증하기 버튼 비활성화
+        buttonCertification.setClickable(false)
+
+        // 뒤로가기 버튼을 눌렀을 경우
+        buttonBack.setOnClickListener {
+            // 이전 화면으로 이동
+            startActivityWithClear(JoinPart1Activity::class.java)
+        }
+
+        // JoinPart1Activity에서 입력받았던 email 값 받기
+        val email = intent.getStringExtra("email")
+        // textviewEmail 값을 위의 email 값으로 변경하기
+        textviewEmail.setText(email)
+        // email textbox 테두리 색 변경하기
+        layoutJoinEmail.setBackgroundResource(R.drawable.box_input_text)
+
+        // 인증코드를 눌렀을 경우 -- textbox 색 변경하기
+        layoutCertificationCode.setBackgroundResource(R.drawable.box_input_text)
+
+        // 인증번호에 문제가 생겼을 경우 -- 해당 버튼 클릭
+        textviewCertificationCodeProblem.setOnClickListener {
+            // 아래에 있던 레이아웃 visible
+            layoutAlert.visibility = View.VISIBLE
+
+            // 인증번호 재전송 버튼을 눌렀을 경우 -- 임시설정
+            //buttonResend.setOnClickListener {  }
+        }
+
+        // 인증코드 확인 여부 -- 임시설정
+        // 인증코드가 일치하는 경우
+        if(true) {
+            // 인증코드가 일치할 경우 -- textbox 색 변경하기
+            layoutCertificationCode.setBackgroundResource(R.drawable.box_input_text)
+
+            // 인증하기 버튼 활성화
+            buttonCertification.setClickable(true)
+            // 인증하기 버튼 색 변경
+            buttonCertification.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.main_theme_blue))
+            // 인증하기 버튼 글자 색 변경
+            buttonCertification.setTextColor(getColor(R.color.white))
+
+            // 인증하기 버튼을 눌렀을 경우
+            buttonCertification.setOnClickListener {
+                // JoinPart3Activity로 화면 전환
+                startNextActivity(JoinPart3Activity::class.java)
+            }
+        }
+        // 인증코드가 불일치하는 경우
+        else {
+            // 인증코드 입력 textbox 색 변경
+            layoutCertificationCode.setBackgroundResource(R.drawable.box_error_text)
+
+            // 잘못된 코드 입력 시 -- error code visible
+            textviewErrorCode.visibility = View.VISIBLE
+        }
+    }
+
+    // timer 함수 구현
+    fun startTimer() = with(binding) {
+       timerTask = timer(period = 10) {
+           time++
+
+           val min = time / 100
+           val sec = time % 60
+
+           runOnUiThread {
+               textviewTimer.setText("${min} : ${sec}")
+           }
+       }
+    }
+}
---
+++ app/src/main/java/com/gachon/ttuckttak/ui/join/JoinPart3Activity.kt
@@ -0,0 +1,91 @@
+package com.gachon.ttuckttak.ui.join
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.gachon.ttuckttak.R
+import com.gachon.ttuckttak.base.BaseActivity
+import com.gachon.ttuckttak.databinding.ActivityJoinPart3Binding
+
+class JoinPart3Activity : BaseActivity<ActivityJoinPart3Binding>(ActivityJoinPart3Binding::inflate) {
+    override fun initAfterBinding() = with(binding) {
+        // 가입하기 버튼 비활성화
+        buttonJoin.setClickable(false)
+        // 비밀번호 입력창 비활성화
+        layoutJoinPassword.setClickable(false)
+        // 비밀번호 확인 입력창 비활성화
+        layoutPasswordCheck.setClickable(false)
+
+        // 뒤로가기 버튼을 눌렀을 경우
+        buttonBack.setOnClickListener {
+            // 이전 화면으로 이동
+            startActivityWithClear(JoinPart2Activity::class.java)
+        }
+
+        // 닉네임 입력창을 눌렀을 경우 -- textbox 색 변경하기
+        layoutJoinName.setBackgroundResource(R.drawable.box_input_text)
+        // 입력받은 닉네임 값 받기
+        val nikname = edittextName.text.toString()
+
+        // 닉네임 중복, 적합 여부 확인 -- 임시설정
+        // 닉네임이 중복이거나 부적합한 경우
+        if(true) {
+            // 닉네임 입력 textbox 색 변경
+            layoutJoinName.setBackgroundResource(R.drawable.box_error_text)
+
+            // error message visible -- 임시설정
+            // 중복된 코드 입력 시
+            if(true) { textviewOverlapName.visibility = View.VISIBLE }
+            // 부적합한 코드 입력 시
+            else { textviewInvalidName.visibility = View.VISIBLE }
+        }
+        else {
+            // 닉네임이 적합하는 경우 -- textbox 색 변경하기
+            layoutJoinName.setBackgroundResource(R.drawable.box_input_text)
+            // 비밀번호 입력창 활성화
+            layoutJoinPassword.setClickable(true)
+            // 비밀번호 입력창을 누르는 경우
+            layoutJoinPassword.setBackgroundResource(R.drawable.box_input_text)
+
+            // 비밀번호 적합 여부 확인 -- 임시설정
+            // 적합하는 경우
+            if(true) {
+                // 사용가능한 비밀번호 visible
+                textviewPasswordUsable.visibility = View.VISIBLE
+
+                // 비밀번호 확인 입력창 활성화
+                layoutPasswordCheck.setClickable(true)
+
+                // 비밀번호 확인 textbox 색 변경
+                layoutPasswordCheck.setBackgroundColor(R.drawable.box_input_text)
+
+                // Q. 비밀번호 일치 여부 확인해야 하는지..?
+
+
+                // 가입하기 버튼 활성화
+                buttonJoin.setClickable(true)
+                // 가입하기 버튼 색 변경
+                buttonJoin.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.main_theme_blue))
+                // 가입하기 버튼 글자 색 변경
+                buttonJoin.setTextColor(getColor(R.color.white))
+
+                // 가입하기 버튼 누르는 경우
+                buttonJoin.setOnClickListener {
+                    // 약관 동의 창 띄우기
+                    layoutAlert.visibility = View.VISIBLE
+                    // 서비스 약관 동의 -- Q.몰르겟어요
+                    radiobuttonAgreeTerms.setOnCheckedChangeListener { buttonView, isChecked -> }
+                }
+            }
+            else {
+                // 비밀번호가 적합하지 않은 경우 -- 비밀번호 적합 조건 visible
+                textviewPasswordError.visibility = View.VISIBLE
+
+                // 비밀번호 textbox 색 변경하기
+                layoutJoinPassword.setBackgroundResource(R.drawable.box_error_text)
+            }
+        }
+
+
+
+    }
+}
