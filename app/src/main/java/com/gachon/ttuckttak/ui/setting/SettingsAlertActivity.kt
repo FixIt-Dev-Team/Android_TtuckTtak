00d41e07b3ed5d4fe18f5260c88d0ee686d54584
yummpotato <hevlyoy26@naver.com>
2023-07-26 00:41:42 +0900

    [UI] 설정 화면 activity 및 xml 수정

--- app/src/main/java/com/gachon/ttuckttak/ui/setting/SettingsAlertActivity.kt
+++ app/src/main/java/com/gachon/ttuckttak/ui/setting/SettingsAlertActivity.kt
@@ -1,5 +1,7 @@
 package com.gachon.ttuckttak.ui.setting
 
+import androidx.core.content.ContextCompat
+import com.gachon.ttuckttak.R
 import com.gachon.ttuckttak.base.BaseActivity
 import com.gachon.ttuckttak.databinding.ActivitySettingsAlertBinding
 
@@ -13,25 +15,26 @@ class SettingsAlertActivity : BaseActivity<ActivitySettingsAlertBinding>(Activit
         }
 
         // event switch를 누르는 경우
-
-        switchEventAndFunctionPush.setOnCheckedChangeListener{_, isChecked ->
-            if(isChecked) {
+        switchEventAndFunctionPush.setOnCheckedChangeListener{CompoundButton, onSwitch ->
+            if(onSwitch) {
                 // switch가 체크된 경우
-                switchEventAndFunctionPush.toggle()
+                switchEventAndFunctionPush.setChecked(true)
             }
             else {
                 // switch가 체크되지 않은 경우
+                switchEventAndFunctionPush.setChecked(false)
             }
         }
 
         // night time switch를 누르는 경우
-        switchNightTimePushAlert.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked) {
+        switchNightTimePushAlert.setOnCheckedChangeListener { CompoundButton, onSwitch ->
+            if(onSwitch) {
                 // switch가 체크된 경우
-                switchNightTimePushAlert.toggle()
+                switchNightTimePushAlert.setChecked(true)
             }
             else {
                 // switch가 체크되지 않은 경우
+                switchEventAndFunctionPush.setChecked(false)
             }
         }
     }
