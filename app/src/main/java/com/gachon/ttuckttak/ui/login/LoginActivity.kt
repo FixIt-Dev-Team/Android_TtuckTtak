c9faedf62b6855edc7c22335444866cb6698108f
CodingNiceTry <rdstar2972@gmail.com>
2023-07-31 18:53:27 +0900

    Fix Login page & find password page

--- app/src/main/java/com/gachon/ttuckttak/ui/login/LoginActivity.kt
+++ app/src/main/java/com/gachon/ttuckttak/ui/login/LoginActivity.kt
@@ -10,14 +10,12 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::i
 
     override fun initAfterBinding() {
         setClickListener()
-        setFocusChangeListener()
-        setTextChangeListener()
     }
 
     private fun setClickListener() = with(binding) {
-        buttonLogin.setOnClickListener {
-            val email = emailEditText.text
-            val pw = pwEditText.text
+        imgButtonLogin.setOnClickListener {
+            val email = editTextEmail.text
+            val pw = editTextPwd.text
 
             // Todo: 서버에 로그인 요청
 
@@ -29,35 +27,35 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::i
         }
 
         textFindIdOrPw.setOnClickListener {
-            // Todo: 아이디 및 비밀번호 찾기 화면 이동
+            startNextActivity(FindPwActivity::class.java)
         }
     }
 
-    private fun setFocusChangeListener() = with(binding) {
-        emailEditText.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                emailInputLayout.error = null
-            }
-
-            else { // 포커스가 해제되었을 때
-                val email = emailEditText.text.toString()
-
-                if (RegexUtil.isValidEmail(email) || email.isBlank()) { // 올바른 이메일 형식이거나 비어 있는 경우
-                    emailInputLayout.error = null
-                } else { // 올바르지 않은 이메일 형식을 입력한 경우
-                    emailInputLayout.error = getString(R.string.invalid_email_format)
-                }
-            }
-        }
-    }
-
-    private fun setTextChangeListener() = with(binding) {
-        pwEditText.doOnTextChanged { pw, _, _, _ ->
-            if (RegexUtil.isValidPw(pw.toString()) || pw.isNullOrBlank()) {
-                pwInputLayout.error = null
-            } else {
-                pwInputLayout.error = getString(R.string.invalid_pw_format)
-            }
-        }
-    }
+//    private fun setFocusChangeListener() = with(binding) {
+//        editTextEmail.setOnFocusChangeListener { _, hasFocus ->
+//            if (hasFocus) {
+//                emailInputLayout.error = null
+//            }
+//
+//            else { // 포커스가 해제되었을 때
+//                val email = emailEditText.text.toString()
+//
+//                if (RegexUtil.isValidEmail(email) || email.isBlank()) { // 올바른 이메일 형식이거나 비어 있는 경우
+//                    emailInputLayout.error = null
+//                } else { // 올바르지 않은 이메일 형식을 입력한 경우
+//                    emailInputLayout.error = getString(R.string.invalid_email_format)
+//                }
+//            }
+//        }
+//    }
+//
+//    private fun setTextChangeListener() = with(binding) {
+//        pwEditText.doOnTextChanged { pw, _, _, _ ->
+//            if (RegexUtil.isValidPw(pw.toString()) || pw.isNullOrBlank()) {
+//                pwInputLayout.error = null
+//            } else {
+//                pwInputLayout.error = getString(R.string.invalid_pw_format)
+//            }
+//        }
+//    }
 }
