99800e98e1321011d962a9ec1dfb0e46dfe1d7ae
SeungHyeon <namsh1125@naver.com>
2023-08-06 13:00:33 +0900

    [Refactor] JoinPart3Activity 가독성 좋게 리팩토링

--- app/src/main/java/com/gachon/ttuckttak/ui/join/JoinPart3Activity.kt
+++ app/src/main/java/com/gachon/ttuckttak/ui/join/JoinPart3Activity.kt
@@ -9,73 +9,74 @@ import com.gachon.ttuckttak.utils.RegexUtil
 
 class JoinPart3Activity : BaseActivity<ActivityJoinPart3Binding>(ActivityJoinPart3Binding::inflate) {
     override fun initAfterBinding() = with(binding) {
+        setClickListener()
+        setFocusChangeListener()
+    }
 
+    private fun setClickListener() = with(binding) {
         // 뒤로가기 버튼을 눌렀을 경우
         imagebuttonBack.setOnClickListener {
             finish()
         }
-        
-        edittextName.setOnFocusChangeListener(object : View.OnFocusChangeListener {
-            override fun onFocusChange(view: View, hasFocus: Boolean) {
-                if (hasFocus) {
-                    edittextName.setBackgroundResource(R.drawable.textbox_state_focused)
-                } else {
-                    val nickname = edittextName.text.toString()
-
-                    if (RegexUtil.isValidNicknameFormat(nickname)) {
-                        edittextName.setBackgroundResource(R.drawable.textbox_state_normal)
-                    } else {
-                        edittextName.setBackgroundResource(R.drawable.textbox_state_error)
-                    }
-                }
-            }
-        })
-
-        edittextPassword.setOnFocusChangeListener(object : View.OnFocusChangeListener {
-            override fun onFocusChange(view: View, hasFocus: Boolean) {
-                if (hasFocus) {
-                    edittextPassword.setBackgroundResource(R.drawable.textbox_state_focused)
-                    textviewPasswordError.visibility = View.INVISIBLE
-                    textviewPasswordRule.visibility = View.INVISIBLE
-                } else {
-                    val password = edittextPassword.text.toString()
-
-                    if (RegexUtil.isValidPwFormat(password)) {
-                        edittextPassword.setBackgroundResource(R.drawable.textbox_state_normal)
-                        textviewPasswordError.visibility = View.INVISIBLE
-                        textviewPasswordUsable.visibility = View.VISIBLE
-                    } else {
-                        edittextPassword.setBackgroundResource(R.drawable.textbox_state_error)
-                        textviewPasswordError.visibility = View.VISIBLE
-                    }
-                }
-            }
-        })
-
-
-        edittextPasswordCheck.setOnFocusChangeListener(object : View.OnFocusChangeListener {
-            override fun onFocusChange(view: View, hasFocus: Boolean) {
-                if (hasFocus) {
-                    edittextPasswordCheck.setBackgroundResource(R.drawable.textbox_state_focused)
-                    textviewPasswordDiscordance.visibility = View.INVISIBLE
-                } else {
-                    val pwCheck = edittextPasswordCheck.text.toString()
-                    val pw = edittextPassword.text.toString()
-
-                    if (pw.equals(pwCheck)) {
-                        edittextPasswordCheck.setBackgroundResource(R.drawable.textbox_state_normal)
-                        textviewPasswordDiscordance.visibility = View.INVISIBLE
-                    } else {
-                        edittextPasswordCheck.setBackgroundResource(R.drawable.textbox_state_error)
-                        textviewPasswordDiscordance.visibility = View.VISIBLE
-                    }
-                }
-            }
-        })
 
         // 가입하기 버튼을 클릭한 경우
         buttonJoin.setOnClickListener {
 
+        }
+    }
+
+    private fun setFocusChangeListener() = with(binding) {
+        edittextName.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                edittextName.setBackgroundResource(R.drawable.textbox_state_focused)
+            } else {
+                val nickname = edittextName.text.toString()
+
+                if (RegexUtil.isValidNicknameFormat(nickname)) {
+                    edittextName.setBackgroundResource(R.drawable.textbox_state_normal)
+                } else {
+                    edittextName.setBackgroundResource(R.drawable.textbox_state_error)
+                }
+            }
+        }
+
+        edittextPassword.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                edittextPassword.setBackgroundResource(R.drawable.textbox_state_focused)
+                textviewPasswordError.visibility = View.INVISIBLE
+                textviewPasswordRule.visibility = View.INVISIBLE
+            } else {
+                val password = edittextPassword.text.toString()
+
+                if (RegexUtil.isValidPwFormat(password)) {
+                    edittextPassword.setBackgroundResource(R.drawable.textbox_state_normal)
+                    textviewPasswordError.visibility = View.INVISIBLE
+                    textviewPasswordUsable.visibility = View.VISIBLE
+                } else {
+                    edittextPassword.setBackgroundResource(R.drawable.textbox_state_error)
+                    textviewPasswordError.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        edittextPasswordCheck.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                edittextPasswordCheck.setBackgroundResource(R.drawable.textbox_state_focused)
+                textviewPasswordDiscordance.visibility = View.INVISIBLE
+            }
+
+            else {
+                val pwCheck = edittextPasswordCheck.text.toString()
+                val pw = edittextPassword.text.toString()
+
+                if (pw == pwCheck) {
+                    edittextPasswordCheck.setBackgroundResource(R.drawable.textbox_state_normal)
+                    textviewPasswordDiscordance.visibility = View.INVISIBLE
+                } else {
+                    edittextPasswordCheck.setBackgroundResource(R.drawable.textbox_state_error)
+                    textviewPasswordDiscordance.visibility = View.VISIBLE
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/gachon/ttuckttak/utils/RegexUtil.kt
+++ app/src/main/java/com/gachon/ttuckttak/utils/RegexUtil.kt
@@ -4,13 +4,6 @@ import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 object RegexUtil {
-    fun isValidEmail(target: String?): Boolean {
-        val regex = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9+-_.]+\\.[a-zA-Z0-9+-_.]+$"
-        val pattern: Pattern = Pattern.compile(regex)
-        val matcher: Matcher = pattern.matcher(target.toString())
-        return matcher.matches()
-    }
-
     fun isValidPwFormat(target: String?): Boolean {
         val regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*\\W).{9,500}\$"
         val pattern: Pattern = Pattern.compile(regex)
