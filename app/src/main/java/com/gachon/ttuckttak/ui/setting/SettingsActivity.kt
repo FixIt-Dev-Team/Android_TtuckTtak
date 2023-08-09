00c05e980ed784dc0eae72be1ac1b05c08fa8161
sjw8793 <JiwonShin@soongsil.ac.kr>
2023-08-09 14:28:05 +0900

    FEAT: 프로필 정보 서버에서 불러오는 함수 추가

--- app/src/main/java/com/gachon/ttuckttak/ui/setting/SettingsActivity.kt
+++ app/src/main/java/com/gachon/ttuckttak/ui/setting/SettingsActivity.kt
@@ -1,10 +1,18 @@
 package com.gachon.ttuckttak.ui.setting
 
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.gachon.ttuckttak.base.BaseActivity
 import com.gachon.ttuckttak.data.local.TokenManager
 import com.gachon.ttuckttak.data.local.UserManager
+import com.gachon.ttuckttak.data.remote.TtukttakServer
+import com.gachon.ttuckttak.data.remote.dto.UserInfoRes
 import com.gachon.ttuckttak.databinding.ActivitySettingsBinding
 import com.gachon.ttuckttak.ui.login.LoginActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 
 class SettingsActivity : BaseActivity<ActivitySettingsBinding>(ActivitySettingsBinding::inflate) {
@@ -13,6 +21,15 @@ class SettingsActivity : BaseActivity<ActivitySettingsBinding>(ActivitySettingsB
     private val tokenManager: TokenManager by lazy { TokenManager(this@SettingsActivity) }
 
     override fun initAfterBinding() = with(binding) {
+        // 서버에서 유저 정보 가져오기
+        getProfile(userManager.getUserIdx()!!, tokenManager.getAccessToken()!!)
+        textviewUserName.text = userManager.getUserName()
+        textviewUserEmail.text = userManager.getUserMail()
+
+        setClickListener()
+    }
+
+    private fun setClickListener() = with(binding) {
         // 뒤로가기 버튼을 누르는 경우
         buttonBack.setOnClickListener {
             finish()
@@ -54,6 +71,40 @@ class SettingsActivity : BaseActivity<ActivitySettingsBinding>(ActivitySettingsB
             tokenManager.clearToken()
             startActivityWithClear(LoginActivity::class.java)
         }
+    }
+
+    private fun getProfile(userId: String, token: String) {
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                val response = TtukttakServer.getUserInfo(userId, token)
+
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccess) {
+                        val data = response.data!!
+                        Log.i(SettingsProfileActivity.TAG, "userName: ${data.userName}")
+                        Log.i(SettingsProfileActivity.TAG, "userMail: ${data.email}")
+                        Log.i(SettingsProfileActivity.TAG, "userImgUrl: ${data.profileImgUrl}")
+                        Log.i(SettingsProfileActivity.TAG, "accountType: ${data.accountType}")
+                        saveProfile(data)
+                    } else {
+                        Log.e(SettingsProfileActivity.TAG, "유저 정보 취득 실패")
+                        Log.e(SettingsProfileActivity.TAG, "${response.code} ${response.message}")
+                        showToast("유저 정보 취득 실패")
+                    }
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Log.e(SettingsProfileActivity.TAG, "서버 통신 오류: ${e.message}")
+                    showToast("유저 정보 취득 실패")
+                }
+            }
+        }
+    }
+
+    private fun saveProfile(data: UserInfoRes) {
+        userManager.saveUserName(data.userName)
+        userManager.saveUserMail(data.email)
+        userManager.saveUserImageUrl(data.profileImgUrl)
     }
 
 }
