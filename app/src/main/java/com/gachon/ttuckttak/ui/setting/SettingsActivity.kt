545e05d7ddb62ee0504496f2830292c6aae27b39
parkhyejeong <hevlyoy26@naver.com>
2023-08-11 01:16:10 +0900

    [FEAT] 로그아웃 API 작성

--- app/src/main/java/com/gachon/ttuckttak/ui/setting/SettingsActivity.kt
+++ app/src/main/java/com/gachon/ttuckttak/ui/setting/SettingsActivity.kt
@@ -1,16 +1,27 @@
 package com.gachon.ttuckttak.ui.setting
 
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.gachon.ttuckttak.R
 import com.gachon.ttuckttak.base.BaseActivity
 import com.gachon.ttuckttak.data.local.TokenManager
 import com.gachon.ttuckttak.data.local.UserManager
+import com.gachon.ttuckttak.data.remote.TtukttakServer
+import com.gachon.ttuckttak.data.remote.dto.LogoutReq
 import com.gachon.ttuckttak.databinding.ActivitySettingsBinding
+import com.gachon.ttuckttak.ui.login.LandingActivity
 import com.gachon.ttuckttak.ui.login.LoginActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class SettingsActivity : BaseActivity<ActivitySettingsBinding>(ActivitySettingsBinding::inflate) {
 
     private val userManager: UserManager by lazy { UserManager(this@SettingsActivity) }
     private val tokenManager: TokenManager by lazy { TokenManager(this@SettingsActivity) }
+
+    private val email: String by lazy { intent.getStringExtra("email")!! }
 
     override fun initAfterBinding() = with(binding) {
         // 뒤로가기 버튼을 누르는 경우
@@ -50,9 +61,37 @@ class SettingsActivity : BaseActivity<ActivitySettingsBinding>(ActivitySettingsB
 
         // 로그아웃 버튼을 누르는 경우 - LoginActivity로 이동
         buttonLogout.setOnClickListener {
-            userManager.clearUserIdx()
-            tokenManager.clearToken()
-            startActivityWithClear(LoginActivity::class.java)
+            lifecycleScope.launch(Dispatchers.IO) {
+                try {
+                    // 서버에 로그아웃 인증코드 전송 요청
+                    val response = TtukttakServer.logout(LogoutReq(userManager.getUserIdx()!!))
+                    Log.i("response", response.toString())
+
+                    if (response.isSuccess) {
+                        userManager.clearUserIdx()
+                        tokenManager.clearToken()
+                        startActivityWithClear(LandingActivity::class.java)
+
+                    } else {
+                        withContext(Dispatchers.Main) {
+                            when (response.code) {
+                                400 -> {
+                                    showToast("아이디나 비밀번호를 확인해주세요.")
+                                }
+
+                                500 -> {
+                                    showToast(getString(R.string.unexpected_error_occurred))
+                                }
+                            }
+                        }
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Log.e(LandingActivity.TAG, "서버 통신 오류: ${e.message}")
+                        showToast("로그아웃 요청 실패")
+                    }
+                }
+            }
         }
     }
 
