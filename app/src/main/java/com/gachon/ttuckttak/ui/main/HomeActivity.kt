09e5f60bdae598fc2e72780ac7bd6dff289708c1
SeungHyeon <namsh1125@naver.com>
2023-08-24 05:42:37 +0900

    [Refactor] 코드 리팩토링 - App Database 모듈 파일 생성

--- app/src/main/java/com/gachon/ttuckttak/ui/main/HomeActivity.kt
+++ app/src/main/java/com/gachon/ttuckttak/ui/main/HomeActivity.kt
@@ -2,16 +2,19 @@ package com.gachon.ttuckttak.ui.main
 
 import android.util.Log
 import com.gachon.ttuckttak.base.BaseActivity
-import com.gachon.ttuckttak.data.local.database.AppDatabase
+import com.gachon.ttuckttak.data.local.dao.DiagnosisDao
 import com.gachon.ttuckttak.databinding.ActivityHomeBinding
 import com.gachon.ttuckttak.ui.problem.ProblemCategoryActivity
 import com.gachon.ttuckttak.ui.setting.SettingsActivity
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::inflate, TransitionMode.HORIZONTAL) {
-    private val database by lazy { AppDatabase.getInstance(this@HomeActivity)!! }
+    @Inject lateinit var diagnosisDao: DiagnosisDao
 
     override fun initAfterBinding() {
         setUi()
@@ -20,8 +23,6 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::infl
 
     private fun setUi() = with(binding) {
         CoroutineScope(Dispatchers.Default).launch {
-            val diagnosisDao = database.diagnosisDao()
-
             // Test
 //            val dateFormat = SimpleDateFormat("yyyy-MM-dd hh:mm")
 //            diagnosisDao.insertDiagnosis(Diagnosis("부팅디스크(SSD) 접촉불량 예상", dateFormat.format(Date(System.currentTimeMillis()))))
