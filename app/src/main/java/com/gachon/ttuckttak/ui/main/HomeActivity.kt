f80fe82f23577115d9638409501b2438edc764fe
parkhyejeong <hevlyoy26@naver.com>
2023-09-03 01:33:34 +0900

    [REFACTOR] 뒤로가기 두번 클릭 시, 앱 종료

--- app/src/main/java/com/gachon/ttuckttak/ui/main/HomeActivity.kt
+++ app/src/main/java/com/gachon/ttuckttak/ui/main/HomeActivity.kt
@@ -1,6 +1,9 @@
 package com.gachon.ttuckttak.ui.main
 
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
 import com.gachon.ttuckttak.base.BaseActivity
 import com.gachon.ttuckttak.databinding.ActivityHomeBinding
 import com.gachon.ttuckttak.ui.problem.ProblemCategoryActivity
@@ -13,10 +16,24 @@ import com.gachon.ttuckttak.ui.main.HomeViewModel.NavigateTo.*
 class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::inflate, TransitionMode.HORIZONTAL) {
 
     private val viewModel: HomeViewModel by viewModels()
+    private var backPressedTime = 0L
 
     override fun initAfterBinding() {
         binding.viewmodel = viewModel
         setObservers()
+        this@HomeActivity.onBackPressedDispatcher.addCallback(this@HomeActivity, onBackPressedCallback)
+    }
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() > backPressedTime + 2000) {
+                backPressedTime = System.currentTimeMillis()
+                showToast("한 번 더 누르면 종료합니다.")
+            } else if (System.currentTimeMillis() <= backPressedTime + 2000) {
+                ActivityCompat.finishAffinity(this@HomeActivity)
+                System.exit(0)
+            }
+        }
     }
 
     private fun setObservers() = with(binding) {
